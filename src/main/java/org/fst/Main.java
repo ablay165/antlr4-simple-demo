870cddf49fcf6019e4c81206e4bc09183a644fee
ablay165 <projavaj2ee@gmail.com>
2025-11-26 11:38:32 +0100

    parse stdin with hello grammar and print parse tree

--- src/main/java/org/fst/Main.java
+++ src/main/java/org/fst/Main.java
@@ -1,8 +1,41 @@
 package org.fst;
 
+import java.io.IOException;
+
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+import org.fst.parser.helloLexer;
+import org.fst.parser.helloParser;
+
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("\t****************\n hello antr4 \n**************");
+    public static void main(String[] args)  throws IOException{ 
+        // create a CharStream that reads from standard input
+        CharStream cs = CharStreams.fromStream(System.in);
+
+
+                // create a lexer that feeds off of input CharStream
+        helloLexer lexer = new helloLexer(cs);
+
+        
+                // create a buffer of tokens pulled from the lexer
+        CommonTokenStream cts = new CommonTokenStream(lexer);
+
+
+                // create a parser that feeds off the tokens buffer
+        helloParser parser = new helloParser(cts);
+
+
+        // begin parsing at r rule
+        ParseTree tree =parser.r();
+
+
+        // print LISP-style tree
+        System.out.println(tree.toStringTree(parser));
+
+
     }
 
 }
