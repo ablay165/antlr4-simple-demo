694a100629ea411b402a8f3931f57429dbd55d42
ablay165 <projavaj2ee@gmail.com>
2025-12-20 11:52:34 +0100

    add JUnit 5 dependency and configure surefire and add simple test

---
+++ src/test/java/org/fst/SimpleTest.java
@@ -0,0 +1,16 @@
+package org.fst;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class SimpleTest {
+    @Test
+    void testAddition(){
+        int result=2;
+        assertEquals(1+1, result);
+    }
+
+
+
+}
